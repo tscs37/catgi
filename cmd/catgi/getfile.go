0271f240f99ccb42d4401b3db080af0c946587cb
Tim Schuster <mail@timschuster.info>
2017-01-01 15:11:03 +0100

    Added RAW Debug Response in JSON Format

--- cmd/catgi/getfile.go
+++ cmd/catgi/getfile.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 	"time"
@@ -61,15 +62,25 @@ func (h *handlerServeGet) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 		}
 	}
 
-	rw.Header().Add("Content-Disposition", "inline; filename="+filename)
-	rw.Header().Add("Content-Type", mimetype)
-	remainingAge := fmt.Sprintf("%.0f", f.DeleteAt.Sub(time.Now().UTC()).Seconds())
-	rw.Header().Add("Cache-Control", "public, max-age="+remainingAge)
-	rw.Header().Add("X-Catgi-Expires-At", f.DeleteAt.Format("2006-01-02"))
-	rw.Header().Add("X-Catgi-Owner", f.User)
-	_, err = rw.Write(f.Data)
+	if r.URL.Query().Get("raw") == "1" {
+		rw.Header().Add("Content-Type", "application/json")
+		var dat []byte
+		dat, err = json.Marshal(f)
+		if err != nil {
+			log.Errorf("Raw output error")
+		}
+		_, err = rw.Write(dat)
+	} else {
+		rw.Header().Add("Content-Disposition", "inline; filename="+filename)
+		rw.Header().Add("Content-Type", mimetype)
+		remainingAge := fmt.Sprintf("%.0f", f.DeleteAt.Sub(time.Now().UTC()).Seconds())
+		rw.Header().Add("Cache-Control", "public, max-age="+remainingAge)
+		rw.Header().Add("X-Catgi-Expires-At", f.DeleteAt.Format("2006-01-02"))
+		rw.Header().Add("X-Catgi-Owner", f.User)
+		_, err = rw.Write(f.Data)
+	}
 	if err != nil {
-		log.Errorf("Error on store: %s", err)
+		log.Errorf("Error on write: %s", err)
 	}
 
 	return
