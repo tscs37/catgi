88cf8188fb864bf76190ae27458ecad0bbd3e532
Tim Schuster <mail@timschuster.info>
2017-01-09 12:59:55 +0100

    Empty Backend for LocalFS

--- backend/localfs/register.go
+++ backend/localfs/register.go
@@ -4,8 +4,9 @@ import (
 	"context"
 
 	"git.timschuster.info/rls.moe/catgi/backend"
-	"git.timschuster.info/rls.moe/catgi/backend/types"
+	"git.timschuster.info/rls.moe/catgi/backend/common"
 	"git.timschuster.info/rls.moe/catgi/logger"
+	"github.com/mitchellh/mapstructure"
 )
 
 const (
@@ -25,6 +26,32 @@ func init() {
 	backend.NewDriver("localfs", NewLocalFSBackend)
 }
 
-func NewLocalFSBackend(params map[string]interface{}, ctx context.Context) (types.Backend, error) {
+func NewLocalFSBackend(params map[string]interface{}, ctx context.Context) (common.Backend, error) {
 	log := logger.LogFromCtx(packageName+".New", ctx)
+
+	var config = &localfsConfig{
+		Root:         "/localfs/",
+		AbsoluteRoot: false,
+	}
+	{
+		log.Debug("Loading Config")
+		decConf := &mapstructure.DecoderConfig{
+			ErrorUnused:      true,
+			WeaklyTypedInput: true,
+			ZeroFields:       false,
+			Result:           config,
+		}
+		decoder, err := mapstructure.NewDecoder(decConf)
+		if err != nil {
+			return nil, err
+		}
+
+		err = decoder.Decode(params)
+		if err != nil {
+			return nil, err
+		}
+		log.Debug("Config Loading Complete")
+	}
+
+	return nil, common.ErrorNotImplemented
 }
