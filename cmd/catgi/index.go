3cac582b95a1c4dcd4f4d607e5b9346c601f13da
Tim Schuster <mail@timschuster.info>
2017-01-31 19:27:56 +0100

    Prefer PWD over FS over Embedded files

--- cmd/catgi/index.go
+++ cmd/catgi/index.go
@@ -9,14 +9,24 @@ import (
 	"git.timschuster.info/rls.moe/catgi/logger"
 )
 
-type handlerServeSite struct{}
+type handlerServeSite struct {
+	rice rice.Config
+}
 
 func newHandlerServeSite() http.Handler {
-	return &handlerServeSite{}
+	return &handlerServeSite{
+		rice: rice.Config{
+			LocateOrder: []rice.LocateMethod{
+				rice.LocateWorkingDirectory,
+				rice.LocateFS,
+				rice.LocateEmbedded,
+			},
+		},
+	}
 }
 func (h *handlerServeSite) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	log := logger.LogFromCtx("serverIndex", r.Context())
-	dat, err := rice.MustFindBox("./resources").Bytes("index.html")
+	dat, err := h.rice.MustFindBox("./resources").Bytes("index.html")
 	if err != nil {
 		log.Error("Could not load file from disk: ", err)
 		rw.WriteHeader(404)
--- cmd/catgi/login.go
+++ cmd/catgi/login.go
@@ -8,15 +8,25 @@ import (
 	"github.com/GeertJohan/go.rice"
 )
 
-type handlerServeLogin struct{}
+type handlerServeLogin struct {
+	rice rice.Config
+}
 
 func newHandlerServeLogin() http.Handler {
-	return &handlerServeLogin{}
+	return &handlerServeLogin{
+		rice: rice.Config{
+			LocateOrder: []rice.LocateMethod{
+				rice.LocateWorkingDirectory,
+				rice.LocateFS,
+				rice.LocateEmbedded,
+			},
+		},
+	}
 }
 
 func (h *handlerServeLogin) ServeHTTP(rw http.ResponseWriter, r *http.Request) {
 	log := logger.LogFromCtx("serveLogin", r.Context())
-	dat, err := rice.MustFindBox("./resources").Bytes("login.html")
+	dat, err := h.rice.MustFindBox("./resources").Bytes("login.html")
 	if err != nil {
 		log.Error("Could not load file from disk or embed: ", err)
 		rw.WriteHeader(404)
